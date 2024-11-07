7c4dd4a06cbd4d8f74fe9e93d7c238515f7988ec
Kimbohy <lovabarwijons@gmail.com>
2024-11-07 09:13:15 +0300

    Add fake publication data and update Home component to render publications dynamically

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,72 @@
 import Publication from "@/components/Publication";
 
+// fake data
+interface PublicationProps {
+  data: {
+    id: string;
+    content: string;
+    image: string;
+    user: {
+      id: string;
+      name: string;
+      avatar: string;
+    };
+    createdAt: string;
+  };
+}
+
+// fake data:
+const publications: PublicationProps[] = [
+  {
+    data: {
+      id: "1",
+      content:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti nesciunt reprehenderit dolore. Non debitis modi, at eaque fugiat, nobis sequi quae dignissimos autem ipsum esse enim. Quaerat praesentium magnam quae.",
+      image: "/hanina.jpg",
+      user: {
+        id: "1",
+        name: "Kimbohy Marisika",
+        avatar: "/avatar.svg",
+      },
+      createdAt: "2j",
+    },
+  },
+  {
+    data: {
+      id: "2",
+      content:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti nesciunt reprehenderit dolore. Non debitis modi, at eaque fugiat, nobis sequi quae dignissimos autem ipsum esse enim. Quaerat praesentium magnam quae.",
+      image: "/hanina.jpg",
+      user: {
+        id: "2",
+        name: "Kimbohy Marisika",
+        avatar: "/avatar.svg",
+      },
+      createdAt: "2j",
+    },
+  },
+  {
+    data: {
+      id: "3",
+      content:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti nesciunt reprehenderit dolore. Non debitis modi, at eaque fugiat, nobis sequi quae dignissimos autem ipsum esse enim. Quaerat praesentium magnam quae.",
+      image: "/hanina.jpg",
+      user: {
+        id: "3",
+        name: "Kimbohy Marisika",
+        avatar: "/avatar.svg",
+      },
+      createdAt: "2j",
+    },
+  },
+];
 export default function Home() {
   return (
     <>
       <div className="flex flex-col gap-2 p-4 items-center ">
-        <Publication />
-        <Publication />
-        <Publication />
+        {publications.map((publication) => (
+          <Publication key={publication.data.id} pub={publication} />
+        ))}
       </div>
     </>
   );
